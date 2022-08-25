456576ee98a2039e6fb8f32cea6892b240ba6305
陈攀宇 <96372678+Chicksqace@users.noreply.github.com>
2022-08-25 13:45:52 +0800

    Add files via upload

---
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -0,0 +1,39 @@
+package cn.ofpp;
+
+import cn.hutool.core.util.StrUtil;
+import cn.ofpp.core.Wx;
+
+/**
+ * 引导配置
+ *
+ * @author DokiYolo
+ * Date 2022-08-22
+ */
+public class Bootstrap {
+
+    /**
+     * 公众号AppID
+     */
+    public static final String APP_ID = "wxd25f6f5e16076503";
+
+    /**
+     * 公众号秘钥
+     */
+    public static final String SECRET = "5564ff8109f99e6aecdb8bc77c60809f";
+
+    /**
+     * 全局模板ID  也可针对单个Friend指定模板
+     */
+    public static final String TEMPLATE_ID = "	7_ZpXB3pAfUShBNRytntgrvMJDgaAyvu0QYNtxI2SWs";
+
+    /**
+     * 初始化
+     */
+    public static void init() {
+        if (StrUtil.hasEmpty(APP_ID, SECRET, TEMPLATE_ID)) {
+            throw new IllegalArgumentException("请检查配置");
+        }
+        Wx.init();
+    }
+
+}
